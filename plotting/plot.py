c1e39ce0205e37ff3a5861f495073b70c0570482
Martin Asenov <m.a.asenov@gmail.com>
2017-03-02 11:42:35 +0000

    plotting the position of the sphere with respect to time

---
+++ plotting/plot.py
@@ -0,0 +1,26 @@
+import csv
+import pylab
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
+x = []
+y = []
+z = []
+with open('../build_cmake/example.txt', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        x.append(float(row[0]))
+        y.append(float(row[1]))
+        z.append(float(row[2]))
+        print row
+
+colors = cm.rainbow(np.linspace(0, 1, len(x)))
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+
+
+ax.scatter(x, y, z, c=colors)
+pylab.show()
